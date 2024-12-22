6bbb99179c010f8ef49113733faed6428698acd4
Kierstyn Hart <stonepreston05@gmail.com>
2024-12-21 18:08:00 -0700

    finalized function and recorded walkthrought video

--- main/src/controllers/thoughtController.ts
+++ main/src/controllers/thoughtController.ts
@@ -8,7 +8,7 @@ export const getThoughts = async (_req: Request, res: Response) => {
     res.json(thoughts);
   } catch (err) {
     res.status(500).json(err);
-  }
+  };
 };
 
 export const getSingleThought = async (req: Request, res: Response) => {
@@ -27,7 +27,7 @@ export const createThought = async (req: Request, res: Response) => {
     res.json(thought);
   } catch (err) {
     res.status(500).json(err);
-  }
+  };
 };
 
 export const updateThought = async (req: Request, res: Response) => {
@@ -36,15 +36,35 @@ export const updateThought = async (req: Request, res: Response) => {
         res.json(thought);
     } catch (err) {
         res.status(500).json(err);
-    }
-}
+    };
+};
 
 export const deleteThought = async (req: Request, res: Response) => {
   try {
     const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
     await User.findOneAndUpdate({ thoughts: req.params.thoughtId }, { $pull: { thoughts: req.params.thoughtId } });
-    res.json(thought);
+    res.status(200).json(`Thought deleted: ${thought}`);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+};
+
+export const postReaction = async (req: Request, res: Response) => {
+  try {
+    const reaction = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $push: { reactions: req.body } }, { new: true });
+    res.status(200).json(`added: ${reaction}`);
   } catch (err) {
     res.status(500).json(err);
   }
+};
+
+export const deleteReaction = async (req: Request, res: Response) => {
+  try {
+    const deletedReaction = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { new: true });
+    res.json(deletedReaction);
+  } catch (err) {
+    if (!res.headersSent) {
+    res.json(err);
+  }
+}
 }
--- main/src/routes/api/thoughtRoutes.ts
+++ main/src/routes/api/thoughtRoutes.ts
@@ -1,11 +1,26 @@
 import { Router } from 'express'
 const router = Router();
-import { getThoughts, getSingleThought, createThought, updateThought, deleteThought } from '../../controllers/thoughtController.js';
+import 
+{ 
+    getThoughts, 
+    getSingleThought, 
+    createThought, 
+    updateThought, 
+    deleteThought,
+    postReaction,
+    deleteReaction 
+} 
+from '../../controllers/thoughtController.js';
 
 // /api/users
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/users/:id
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(postReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 export default router
